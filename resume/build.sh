b5e4f73ac7fae5f87e3798f6deb56877d038085c
Norman Bui <75687022+nbui23@users.noreply.github.com>
2026-04-26 16:13:47 -0400

    More small resume changes

--- resume/build.sh
+++ resume/build.sh
@@ -2,15 +2,20 @@
 set -e
 cd "$(dirname "$0")"
 
+mkdir -p swe aiml qa
+
 echo "Building all resume variants..."
 
-pdflatex -interaction=nonstopmode -jobname="Norman_Bui_Resume"      general.tex
-pdflatex -interaction=nonstopmode -jobname="Norman_Bui_Resume_SWE"  swe.tex
-pdflatex -interaction=nonstopmode -jobname="Norman_Bui_Resume_AIML" aiml.tex
-pdflatex -interaction=nonstopmode -jobname="Norman_Bui_Resume_QA"   qa.tex
+pdflatex -interaction=nonstopmode -jobname="Norman_Bui_Resume" general.tex
+pdflatex -interaction=nonstopmode -output-directory=swe  -jobname="Norman_Bui_Resume" swe.tex
+pdflatex -interaction=nonstopmode -output-directory=aiml -jobname="Norman_Bui_Resume" aiml.tex
+pdflatex -interaction=nonstopmode -output-directory=qa   -jobname="Norman_Bui_Resume" qa.tex
 
 rm -f *.aux *.log *.out *.synctex.gz
+rm -f swe/*.aux  swe/*.log  swe/*.out  swe/*.synctex.gz
+rm -f aiml/*.aux aiml/*.log aiml/*.out aiml/*.synctex.gz
+rm -f qa/*.aux   qa/*.log   qa/*.out   qa/*.synctex.gz
 
 echo ""
-echo "Done. PDFs generated:"
-ls -1 *.pdf
+echo "Done. PDFs:"
+ls -1 Norman_Bui_Resume.pdf swe/Norman_Bui_Resume.pdf aiml/Norman_Bui_Resume.pdf qa/Norman_Bui_Resume.pdf
